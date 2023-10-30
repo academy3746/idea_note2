fd818a48f8619a707b6147fd1c9a1f530d01ca37
ijonghyeon <academy3746@gmail.com>
2023-10-30 18:35:59 +0900

    4. 4th Raw data

--- lib/features/screens/splash_screen.dart
+++ lib/features/screens/splash_screen.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:idea_note2/constants/gaps.dart';
+import 'package:idea_note2/constants/sizes.dart';
+import 'package:idea_note2/features/screens/main_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   static String routeName = "/";
+
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, MainScreen.routeName);
+    });
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(
+          Sizes.size20,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/splash.png",
+                width: Sizes.size150 + Sizes.size30,
+                height: Sizes.size150 + Sizes.size30,
+              ),
+              Gaps.v32,
+              const Text(
+                "아이디어노트",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
